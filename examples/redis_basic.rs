664e2f06a064c7053484de3a33c74fddc508bd15
daheige <zhuwei313@hotmail.com>
2023-08-12 14:38:15 +0800

    feat: redis client async support and update tokio version

--- examples/redis_basic.rs
+++ examples/redis_basic.rs
@@ -16,3 +16,27 @@ fn main() {
         println!("set success");
     }
 }
+
+// async exec redis operation
+#[tokio::test]
+async fn redis_async_test() -> redis::RedisResult<()> {
+    // you must use this module for async exec.
+    use redis::AsyncCommands;
+    let dsn = "redis://:@127.0.0.1:6379/0";
+    let client = RedisConf::builder().with_dsn(dsn).client().unwrap();
+    let mut conn = client.get_async_connection().await?;
+    conn.set("user1", "daheige").await?;
+
+    // async exec set cmd
+    redis::cmd("set")
+        .arg(&["user2", "foo"])
+        .query_async(&mut conn)
+        .await?;
+
+    let result = redis::cmd("mget")
+        .arg(&["user1", "user2"])
+        .query_async(&mut conn)
+        .await;
+    assert_eq!(result, Ok(("daheige".to_string(), "foo".to_string())));
+    Ok(())
+}
